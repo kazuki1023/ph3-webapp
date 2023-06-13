dec1c340ea0899af755f6bb89e515c96efa76135
kazuki1023 <k.keio12569@gmail.com>
2023-06-14 02:31:12 +0900

    add 見た目完成

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -1,17 +1,37 @@
+{{-- webapp --}}
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
+    <x-slot name="slot">
+        {{-- 各card.blade.phpを呼び出したい --}}
+        @include('layouts.card', [
+            'title' => '今日',
+            'description' => 'Card Description',
+        ])
+        @include('layouts.card', [
+            'title' => '今月',
+            'description' => 'Card Description',
+        ])
+        @include('layouts.card', [
+            'title' => '合計',
+            'description' => 'Card Description',
+        ])
+    </x-slot>
+    <x-slot name="paiChart">
+        {{-- 各card.blade.phpを呼び出したい --}}
+        @include('layouts.paiChart', [
+            'subTitle' => 'コンテンツ学習時間',
+        ])
+        @include('layouts.paiChart', [
+            'subTitle' => '学習時間',
+        ])
+    </x-slot>
+    <x-slot name="barChart">
+        {{-- 各card.blade.phpを呼び出したい --}}
+        @include('layouts.barChart')
+    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
 </x-app-layout>
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,36 +1,49 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        @include('layouts.navigation')
+
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-white dark:bg-gray-800 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
+
+        <!-- Page Content -->
+        <main class="relative max-w-[1100px] mx-auto grid gap-10 pt-50 grid-cols-2 ">
+            <div class="grid gap-10 min-w-[400px]">
+                <ul class="grid grid-cols-3 gap-6 h-[160px]">
+                    {{ $slot }}
+                </ul>
+                <div class="h-[400px]  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+                    {{ $barChart }}
+                </div>
+            </div>
+            <div class="grid grid-cols-2 gap-4 h-[600px] ">
+                {{ $paiChart }}
+            </div>
+        </main>
+    </div>
+</body>
+
 </html>
