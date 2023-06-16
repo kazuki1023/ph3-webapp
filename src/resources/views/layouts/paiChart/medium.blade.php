b999e32e23735db2f5fd4a8e281381483b366142
kazuki1023 <k.keio12569@gmail.com>
2023-06-16 19:24:33 +0900

    add ファイル構成変更

---
+++ src/resources/views/layouts/paiChart/medium.blade.php
@@ -0,0 +1,61 @@
+@props([
+    'subTitle' => 'subTitle',
+    'paiChart' => 'paiChart',
+    'mediumHourData' => 'mediumHourData',
+])
+<div class="max-w-sm py-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <p>{{ $subTitle }}</p>
+    {{-- paiChart挿入する --}}
+    <div id="{{ $paiChart }}" class="mx-auto"></div>
+</div>
+<script>
+    var hourData = <?php echo json_encode($mediumHourData, JSON_UNESCAPED_UNICODE); ?>;
+
+    var contents = [];
+    var hours = [];
+
+    hourData.forEach(function(item) {
+        contents.push(item.content);
+        hours.push(parseFloat(item.total_hour));
+    });
+    var options = {
+        series: hours,
+        labels: contents,
+        chart: {
+            width: 270,
+            type: 'donut',
+        },
+        plotOptions: {
+            pie: {
+                startAngle: 0,
+                endAngle: 360
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        fill: {
+            type: 'gradient',
+        },
+        legend: {
+            position: 'bottom',
+            formatter: function(val, opts) {
+                return val + " - " + opts.w.globals.series[opts.seriesIndex]
+            }
+        },
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 200
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }]
+    };
+
+    var chart = new ApexCharts(document.getElementById("{{ $paiChart }}"), options);
+    chart.render();
+</script>
