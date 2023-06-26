41af22de2e88dd9fd795a3434c1249c3af362c6c
kazuki1023 <k.keio12569@gmail.com>
2023-06-26 22:47:37 +0900

    スタイル調整

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -69,7 +69,7 @@
             <!-- Modal body -->
             <form action="{{ route("dashboard.store")}}" method="POST" enctype="multipart/form-data">
                 @csrf
-                <div class="p-6 flex ">
+                <div class="p-5 flex ">
                     <div class="text-base leading-relaxed text-gray-500 dark:text-gray-400 w-1/2">
                         {{-- 学習日 --}}
                         <div class="relative max-w-sm">
@@ -83,7 +83,7 @@
                             <div class="flex flex-wrap gap-1">
                                 @foreach ($languages as $language)
                                     <div class="modal_lang w-max">
-                                        <label for="lang_{{ $language->id }}"class="lang inline-block"></label>
+                                        <label for="lang_{{ $language->id }}"class="lang"></label>
                                         <input type="checkbox" id="lang_{{ $language->id }}"
                                             value="{{ $language->id }}" name="lang[]">{{ $language->name }}
                                     </div>
@@ -107,8 +107,8 @@
                     <div class="text-base leading-relaxed text-gray-500 dark:text-gray-400 w-1/2 ">
                         {{-- 学習時間 --}}
                         <div class="mb-6">
-                            <label for="studyHours"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">学習時間</label>
+                            <h4 for="studyHours"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">学習時間</h4>
                             <select id="studyHours"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="studyHours">
                                 <option selected>学習時間を選択してね</option>
@@ -119,8 +119,8 @@
                         </div>
                         {{-- コメント --}}
                         <div class="">
-                            <label for="message"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">コメント</label>
+                            <h4 for="message"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">コメント</h4>
                             <textarea id="message" rows="4"
                                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="コメントあれば"></textarea>
@@ -129,7 +129,7 @@
                 </div>
                 <!-- Modal footer -->
                 <div
-                    class="flex items-center space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600 justify-center">
+                    class="flex items-center space-x-2 border-gray-200 rounded-b dark:border-gray-600 justify-center mb-1">
                     <button data-modal-hide="defaultModal" type="submit"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">登録</button>
                 </div>
